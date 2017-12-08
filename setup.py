441058e230c5aef2702808fa1e297afdf0d9590b
Ryan Harter <harterrt@gmail.com>
2017-12-08 16:53:22 -0500

    Add setup.py and tox config boilerplate

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+test_deps = [
+    'coverage',
+    'pytest-cov',
+    'pytest',
+]
+
+extras = {
+    'testing': test_deps,
+}
+
+setup(
+    name='stmocli',
+    version='0.1',
+    description='CLI for managing re:dash queries in plain text.',
+    author='Ryan Harter',
+    author_email='harterrt@mozilla.com',
+    url='https://github.com/harterrt/stmocli',
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    install_requires=[
+        "redash-client",
+    ],
+    tests_require=test_deps,
+    extras_require=extras,
+)
