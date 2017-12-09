18daf079ac0ffa8e29a65ee6e62caf676ce02ccf
Ryan Harter <harterrt@gmail.com>
2017-12-09 11:00:35 -0500

    Make tests pass for track command
    Removing py35 from our compatibility testing. I can't tell whether
    redash_client supports python 3 or not. When loading the json at line 82
    [1], we get errors because the string is encoded as bytes. We should
    decode before reading the json.
    
    [1] https://github.com/mozilla/redash_client/blob/master/redash_client/client.py#L82

--- stmocli/cli.py
+++ stmocli/cli.py
@@ -1,17 +1,41 @@
+from redash_client.client import RedashClient
+from requests.compat import urljoin
 import click
+import os
+import requests
+
 
 @click.group()
 def cli():
     pass
+
 
 @cli.command()
 def init():
     with open('.stmocli.conf', 'a'):
         pass
 
+
 @cli.command()
-def track():
-    pass
+@click.argument('query_id')
+@click.argument('filename')
+@click.option(
+    '--redash_api_key',
+    default=lambda: os.environ.get('REDASH_API_KEY', '')
+)
+def track(query_id, filename, redash_api_key):
+    # Get query: https://github.com/getredash/redash/blob/1573e06e710733714d47940cc1cb196b8116f670/redash/handlers/api.py#L74
+    redash = RedashClient(redash_api_key)
+    url_path = 'queries/{}?api_key={}'.format(query_id, redash_api_key)
+    results, response = redash._make_request(
+        requests.get, 
+        urljoin(redash.BASE_URL, url_path)
+    )
+    query = results['query']
+
+    with open(filename, 'w') as outfile:
+        outfile.write(query)
+
 
 if __name__ == '__main__':
     cli()
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,19 +1,26 @@
+import traceback
 import pytest
 import click
 import os
+from httmock import all_requests, HTTMock
 from redash_client.client import RedashClient
 from click.testing import CliRunner
 from stmocli import cli
 
 
+with open('tests/data/example_query_response.json', 'rt') as infile:
+    example_response = infile.read()
+
+@all_requests
+def response_content(url, request):
+    return {
+        'status_code': 200,
+        'content': example_response,
+    }
+
 @pytest.fixture
 def runner():
     return CliRunner()
-
-# @pytest.fixture
-# def redash():
-#     return RedashClient("SUPERFAKEAPIKEY")
-
 
 def test_init(runner):
     with runner.isolated_filesystem():
@@ -22,16 +29,28 @@ def test_init(runner):
         assert os.path.isfile('.stmocli.conf')
 
 def test_track(runner):
-    # gonna need this: https://docs.pytest.org/en/latest/monkeypatch.html
+    query_id = '49741'
     filename = 'poc.sql'
-    query_id = 49741
 
     with runner.isolated_filesystem():
-        runner.invoke(cli.track, [
-            query_id,
-            filename,
-        ])
+        with HTTMock(response_content):
+            result = runner.invoke(cli.track, [
+                query_id,
+                filename,
+                '--redash_api_key',
+                'TOTALLY_FAKE_KEY'
+            ])
+
 
         assert os.path.isfile(filename)
         with open(filename, 'r') as query:
             assert "SELECT" in query.read()
+
+
+def print_debug_for_runner(result):
+    """Helper function for printing click.runner debug tracebacks."""
+    print(result.output)
+    print(result.exc_info)
+    traceback.print_tb(result.exc_info[2])
+    print(result.exit_code)
+    print(result.exception)
