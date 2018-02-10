3bbc85d295b3652d2dd1842f859106f0ca9cd23b
Mark Reid <mreid@mozilla.com>
2018-02-09 20:55:56 -0400

    Add test coverage for "push".
    Output success/fail messages, add test coverage.

--- stmocli/cli.py
+++ stmocli/cli.py
@@ -2,6 +2,7 @@ from redash_client.client import RedashClient
 from requests.compat import urljoin
 from .conf import Conf
 import click
+import md5
 import os
 import requests
 
@@ -32,24 +33,28 @@ def track(conf, query_id, file_name, redash_api_key):
     # Get query: https://github.com/getredash/redash/blob/1573e06e710733714d47940cc1cb196b8116f670/redash/handlers/api.py#L74
     redash = RedashClient(redash_api_key)
     url_path = 'queries/{}?api_key={}'.format(query_id, redash_api_key)
-    results, response = redash._make_request(
-        requests.get,
-        urljoin(redash.API_BASE_URL, url_path)
-    )
+    try:
+        results, response = redash._make_request(
+            requests.get,
+            urljoin(redash.API_BASE_URL, url_path)
+        )
 
-    query = results['query']
-    with open(file_name, 'w') as outfile:
-        outfile.write(query)
+        query = results['query']
+        with open(file_name, 'w') as outfile:
+            outfile.write(query)
 
-    query_meta = {
-        "query_id": query_id,
-        "data_source_id": results['data_source_id'],
-        "name": results['name'],
-        "description": results['description'],
-        "schedule": results['schedule'],
-        "options": results['options']
-    }
-    conf.add_query(file_name, query_meta)
+        query_meta = {
+            "id": query_id,
+            "data_source_id": results['data_source_id'],
+            "name": results['name'],
+            "description": results['description'],
+            "schedule": results['schedule'],
+            "options": results['options']
+        }
+        conf.add_query(file_name, query_meta)
+        click.echo("Tracking Query ID {} in {}".format(query_id, file_name))
+    except RedashClient.RedashClientException as e:
+        click.echo("Failed to track Query ID {}: {}".format(query_id, e))
 
 
 @cli.command()
@@ -62,14 +67,24 @@ def track(conf, query_id, file_name, redash_api_key):
 def push(conf, file_name, redash_api_key):
     redash = RedashClient(redash_api_key)
 
-    query_meta = conf.get_query(file_name)
-    with open(file_name, 'r') as fin:
-        query = fin.read()
+    try:
+        meta = conf.get_query(file_name)
+        with open(file_name, 'r') as fin:
+            query = fin.read()
 
-    redash.update_query(query_meta['query_id'], query_meta['name'],
-                        query, query_meta['data_source_id'],
-                        query_meta['description'], query_meta['options'])
-
+        try:
+            redash.update_query(meta['id'], meta['name'],
+                                query, meta['data_source_id'],
+                                meta['description'], meta['options'])
+            m = md5.new()
+            m.update(query)
+            click.echo("Query ID {} updated with content from {} (md5 {})".format(
+                meta["id"], file_name, m.hexdigest()))
+        except RedashClient.RedashClientException as e:
+            click.echo("Failed to update query from {}: {}".format(file_name, e))
+    except KeyError as e:
+        click.echo("Failed to update query from {}: No such query, "
+                   "maybe you need to 'track' first".format(file_name))
 
 
 if __name__ == '__main__':
