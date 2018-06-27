37f57e70dfb963dc2c12db1f6beab024428f39a9
Tim Smith <tdsmith@mozilla.com>
2018-06-27 15:26:29 -0700

    Add docstrings

--- stmocli/cli.py
+++ stmocli/cli.py
@@ -8,19 +8,27 @@ from .stmo import STMO
 from .util import name_to_stub
 
 
-@click.group()
+@click.group(context_settings={'help_option_names': ['-h', '--help']})
 @click.option(
     '--redash_api_key',
-    default=lambda: os.environ.get('REDASH_API_KEY', '')
+    default=lambda: os.environ.get('REDASH_API_KEY', ''),
+    help=("A redash user API key, from your user settings page. "
+          "Defaults to the value of the REDASH_API_KEY environment variable.")
 )
 @click.pass_context
 def cli(ctx, redash_api_key):
+    """St. Mocli is a command-line interface for sql.telemetry.mozilla.org."""
     ctx.obj = STMO(redash_api_key)
 
 
 @cli.command()
 @click.pass_obj
 def init(stmo):
+    """Initializes a repository.
+
+    Creates an empty .stmocli.conf in the current directory, which will hold
+    stmocli's metadata.
+    """
     stmo.conf.init_file()
 
 
@@ -29,6 +37,16 @@ def init(stmo):
 @click.argument('query_id')
 @click.argument('file_name', required=False)
 def track(stmo, query_id, file_name):
+    """Adds a STMO query to the local repository.
+
+    QUERY_ID: The numeric ID of the redash query you wish to track
+
+    FILE_NAME: The filename to use for the query SQL in the local repository.
+    You will be prompted with a suggested filename if you don't provide one.
+
+    Downloads the SQL query associated with the given query ID and saves it in a file
+    with the given name.
+    """
     def make_file_name(query):
         if file_name:
             return file_name
@@ -50,6 +68,12 @@ def track(stmo, query_id, file_name):
 @click.pass_obj
 @click.argument('file_name')
 def push(stmo, file_name):
+    """Uploads a tracked query to STMO.
+
+    FILE_NAME: The filename of the tracked query SQL.
+
+    Overwrites the STMO query SQL with the version in the local repository.
+    """
     try:
         queryinfo = stmo.push_query(file_name)
     except stmo.RedashClientException as e:
@@ -72,6 +96,12 @@ def push(stmo, file_name):
 @click.pass_obj
 @click.argument('file_name')
 def view(stmo, file_name):
+    """Opens a query in a browser.
+
+    FILE_NAME: The filename of the tracked query SQL.
+
+    Opens a browser window to the redash query.
+    """
     try:
         url = stmo.url_for_query(file_name)
     except KeyError:
@@ -87,6 +117,13 @@ def view(stmo, file_name):
 @click.argument('query_to_fork')
 @click.argument('new_query_file_name')
 def fork(stmo, query_to_fork, new_query_file_name):
+    """Copies a STMO  query and tracks the result.
+
+    QUERY_TO_FORK: Either the filename of a query that's already tracked,
+    or the numeric ID of any redash query.
+
+    NEW_QUERY_FILE_NAME: The filename to use for the new query's SQL.
+    """
     if os.path.exists(query_to_fork):
         try:
             query_id = stmo.conf.get_query(query_to_fork).id
